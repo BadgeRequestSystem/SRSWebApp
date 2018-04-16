9669ec0e1415c7994f3802da809df3c47ac7e636
Cliett <36900974+Cliett@users.noreply.github.com>
2018-04-16 03:13:49 -0400

    Issues on Salted Hashes
    The queries for login are not working correctly. I don't know if i made a logicol error, or if my internet is just acting up and preventing the connection from working right.
    
    I wanted to let someone else take a look. If you bypass the first query to get the hash, and uncomment //byte[] hashBytes = Convert.FromBase64String("4Nio0GMUEVdeRJ1vkS6Yog0rWUkqTZ/uOLGIds4KAodN1gMr");
    
    it will execute to marker 6 and then fail on the next query it is asked to make and never get to marker 7. (Note that the markers will stop execution if they are hit since they change the username text and will mess up the next query in line)

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -5,6 +5,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Net;
+using System.Security.Cryptography;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -12,7 +13,17 @@ public partial class Login : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //Currently just used to generate password hashes...
+        //byte[] salt;
+        //new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+        //System.Security.Cryptography.Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes("Adams", salt, 10000);
+        //byte[] hash = pbkdf2.GetBytes(20);
+        //byte[] hashBytes = new byte[36];
+        //Array.Copy(salt, 0, hashBytes, 0, 16);
+        //Array.Copy(hash, 0, hashBytes, 16, 20);
+        //string savedPasswordHash = Convert.ToBase64String(hashBytes);
+        //userBox.Text = savedPasswordHash; //+ " Salt:" + Convert.ToBase64String(salt);
+        
     }
 
     protected void LoginBtn_Click(object sender, EventArgs e)
@@ -26,37 +37,61 @@ public partial class Login : System.Web.UI.Page
         {
             try
             {
-                Connection.Open();
-                SqlCommand cmd = new SqlCommand(@"SELECT Count(*) FROM Credentials 
-                                        WHERE Username=@uname and 
-                                        Password=@pass", Connection);
+                //Connection.Open();
+                //SqlCommand cmd = new SqlCommand(@"SELECT Count(*) FROM Credentials 
+                //                        WHERE Username=@uname and 
+                //                        Password=@pass", Connection);
+                //cmd.Parameters.AddWithValue("@uname", userBox.Text);
+                //cmd.Parameters.AddWithValue("@pass", passBox.Text);
+
+                //int result = (int)cmd.ExecuteScalar();
+
+
+                //userBox.Text = "Bang1";
+                SqlCommand cmd = new SqlCommand(@"Select PasswordHash FROM Credentials
+                                                    WHERE Username=@uname", Connection);
                 cmd.Parameters.AddWithValue("@uname", userBox.Text);
-                cmd.Parameters.AddWithValue("@pass", passBox.Text);
 
-                int result = (int)cmd.ExecuteScalar();
-
-                if (result > 0) //LOGIN SUCCESFUL
+                string temp2 = (string)cmd.ExecuteScalar();
+                //userBox.Text = "Bang2";
+                byte[] hashBytes = Convert.FromBase64String(temp2);
+                //byte[] hashBytes = Convert.FromBase64String("4Nio0GMUEVdeRJ1vkS6Yog0rWUkqTZ/uOLGIds4KAodN1gMr");
+                byte[] salt = new byte[16];
+                Array.Copy(hashBytes, 0, salt, 0, 16);
+                System.Security.Cryptography.Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(passBox.Text, salt, 10000);
+                byte[] hash = pbkdf2.GetBytes(20);
+                //userBox.Text = "Bang3";
+                int result = 1;
+                for (int i = 0; i < 20; i++)
+                    if (hashBytes[i + 16] != hash[i])
+                    {
+                        //userBox.Text = "Bang4";
+                        result = 0;
+                    }
+                //userBox.Text = "Bang5";
+                //if (result > 0) //LOGIN SUCCESFUL
+                if (result == 1)
                 {
-
+                    //userBox.Text = "Bang6";
                     //logged in succesfully, storing isManager and isHR to fields and then to cookie
                     SqlCommand cmdGetManager = new SqlCommand(@"SELECT isManager FROM Credentials 
-                                                    WHERE Username=@uname and Password=@pass", Connection);
+                                                    WHERE Username=@uname", Connection);
                     cmdGetManager.Parameters.AddWithValue("@uname", userBox.Text);
-                    cmdGetManager.Parameters.AddWithValue("@pass", passBox.Text);
+                    //cmdGetManager.Parameters.AddWithValue("@pass", passBox.Text);
                     bool temp = (bool)cmdGetManager.ExecuteScalar();
                     isManager = temp;
-
+                    //userBox.Text = "Bang7";
                     SqlCommand cmdGetHR = new SqlCommand(@"SELECT isHR FROM Credentials 
-                                                    WHERE Username=@uname and Password=@pass", Connection);
+                                                    WHERE Username=@uname", Connection);
                     cmdGetHR.Parameters.AddWithValue("@uname", userBox.Text);
-                    cmdGetHR.Parameters.AddWithValue("@pass", passBox.Text);
+                    //cmdGetHR.Parameters.AddWithValue("@pass", passBox.Text);
                     temp = (bool)cmdGetHR.ExecuteScalar();
                     isHR = temp;
-
+                    
                     SqlCommand cmdGetUserID = new SqlCommand(@"SELECT UserID FROM Credentials 
-                                                    WHERE Username=@uname and Password=@pass", Connection);
+                                                    WHERE Username=@uname", Connection);
                     cmdGetUserID.Parameters.AddWithValue("@uname", userBox.Text);
-                    cmdGetUserID.Parameters.AddWithValue("@pass", passBox.Text);
+                    //cmdGetUserID.Parameters.AddWithValue("@pass", passBox.Text);
                     tempID = (string)cmdGetUserID.ExecuteScalar();
 
                     SqlCommand cmdGetManagerName = new SqlCommand(@"SELECT [Manager Name] FROM Employees
@@ -65,9 +100,9 @@ public partial class Login : System.Web.UI.Page
                     tempMan = (string)cmdGetManagerName.ExecuteScalar();
 
                     SqlCommand cmdGetEmail = new SqlCommand(@"SELECT Email FROM Credentials
-                                                    WHERE Username=@uname and Password=@pass", Connection);
+                                                    WHERE Username=@uname", Connection);
                     cmdGetEmail.Parameters.AddWithValue("@uname", userBox.Text);
-                    cmdGetEmail.Parameters.AddWithValue("@pass", passBox.Text);
+                    //cmdGetEmail.Parameters.AddWithValue("@pass", passBox.Text);
                     tempEmail = (string)cmdGetEmail.ExecuteScalar();
 
                     HttpCookie aCookie = new HttpCookie("userInfo");
