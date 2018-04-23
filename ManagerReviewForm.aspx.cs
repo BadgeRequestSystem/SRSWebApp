5841528a8110f6986813d9e208b53184a16736dd
Cliett <36900974+Cliett@users.noreply.github.com>
2018-04-23 08:40:19 -0400

    Sends Emails for review request
    Need to change google to our login page.

--- ManagerReviewForm.aspx.cs
+++ ManagerReviewForm.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 public partial class ManagerReviewForm : System.Web.UI.Page
 {
@@ -69,6 +70,7 @@ public partial class ManagerReviewForm : System.Web.UI.Page
             cmd.ExecuteNonQuery();
             Connection.Close();
         }
+        sendNotification(EmployeeDDL.Text, "Need");
         Response.Redirect("~/PendingActionForm.aspx");
 
 
@@ -86,6 +88,7 @@ public partial class ManagerReviewForm : System.Web.UI.Page
             cmd.ExecuteNonQuery();
             Connection.Close();
         }
+        sendNotification(EmployeeDDL.Text, "Approved");
         Response.Redirect("~/PendingActionForm.aspx");
     }
 
@@ -101,6 +104,78 @@ public partial class ManagerReviewForm : System.Web.UI.Page
             cmd.ExecuteNonQuery();
             Connection.Close();
         }
+        sendNotification(EmployeeDDL.Text, "Denied");
         Response.Redirect("~/PendingActionForm.aspx");
+    }
+
+    public void sendNotification(string Employee, string Response)
+    {
+        try
+        {
+            if (Response != "Need")
+            {
+                //Response should be either "Accepted" or "Denied"
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("dontreplysrsmail@gmail.com");
+                mail.To.Add(returnEmail(Employee));
+                mail.Subject = String.Format("SRS Badge Request {0}", Response);
+                mail.IsBodyHtml = true;
+                string body1 = String.Format("Dear {0},\n\tWe wanted to inform you that your SRS Badge Request has been {1}." + "\nWe thank you for your patience. \nSincerely,\nThe SRS Badge Request System\n", Employee, Response);
+                mail.Body = body1 + "Please click " + "<a href = 'https://google.com' > HERE </a>" + "if you would like to vist the SRS Badge Request Site.";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("dontreplysrsmail@gmail.com", "Password!1");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            else
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("dontreplysrsmail@gmail.com");
+                mail.To.Add(returnEmail(Employee));
+                mail.Subject = String.Format("SRS Badge Request Needs Review");
+                mail.IsBodyHtml = true;
+                string body1 = String.Format("Dear {0},\n\tWe wanted to inform you that your SRS Badge Request has been flagged due to an error." + "\nPlease review your pending requests and make any needed corrections.\nSincerely,\nThe SRS Badge Request System\n", Employee);
+                mail.Body = body1 + "Please click " + "<a href = 'https://google.com' > HERE </a>" + "if you would like to vist the SRS Badge Request Site.";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("dontreplysrsmail@gmail.com", "Password!1");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+
+    }
+
+
+
+
+    public string returnEmail(string Employee)
+    {
+        HttpCookie aCookie = Request.Cookies["userInfo"];
+        string email = string.Empty;
+        using (SqlConnection Connection = new SqlConnection("Data Source=badgerequest.cthyx0iu4w46.us-east-2.rds.amazonaws.com;Initial Catalog=badge_request;User ID=pwndatnerd;Password=AaronDavidRandall!3"))
+        {
+            Connection.Open();
+            SqlCommand cmd = new SqlCommand(@"Select Email FROM Credentials WHERE UserID =
+                                            (Select UserID FROM Employees WHERE ([First Name] + ' ' + [Middle Name]  + ' ' + [Last Name]) =@employee)", Connection);
+            cmd.Parameters.AddWithValue("@employee", Employee);
+            email = (string)cmd.ExecuteScalar();
+        }
+
+
+        return email;
+
     }
 }
