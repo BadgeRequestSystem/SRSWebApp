0dfdac519d07cfbf73ffec3957e1a190d9056aae
Aaron Prather <aaprather@augusta.edu>
2018-05-16 13:13:49 -0400

    EditEmployeeForum 'SubmmitButton_Click' has been reduced in size and calls a single method in the 'Methods' class
    Need to add graceful error for when invalid input is submitted (this was an issue during our presentation)

--- App_Code/Methods.cs
+++ App_Code/Methods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Web;
 
 /// <summary>
@@ -7,11 +8,13 @@ using System.Web;
 /// </summary>
 public class Methods
 {
+    //So we won't have the connection string hanging around every page and changing it here will change it everywhere!
+    public string SQL_STRING = "Data Source=badgerequest.cthyx0iu4w46.us-east-2.rds.amazonaws.com;Initial Catalog=badge_request;User ID=pwndatnerd;Password=AaronDavidRandall!3";
+    //~~
+
     public Methods()
     {
-        //
-        // TODO: Add constructor logic here
-        //
+
     }
 
     public string sanitizeInput(string x)
@@ -30,8 +33,49 @@ public class Methods
     public string lastFourOnly(string x)
     {
         string toReturn = "*****" + x[5] + x[6] + x[7] + x[8];
-        
+
         return toReturn;
 
     }
+
+    public void EditEmployee(string CASE, string LName, string FName, string MName, string Initials, string UserID, string ECompany, string Department, string WLocation, string WPhone, string MWLocation, string MWPhone, string EManager)
+    {
+        SqlCommand query = new SqlCommand(); //store the command here
+
+        using (SqlConnection Connection = new SqlConnection(SQL_STRING))
+        {
+            if (CASE == "NEW")
+                query = new SqlCommand(@"INSERT INTO Employees
+                    VALUES (@LName, @FName, @MName, @Initials, @UserID, @ECompany, @Department, @WLocation, @WPhone, @EManager, @MWLocation, @MWPhone);", Connection);
+            if (CASE == "UPDATE")
+                query = new SqlCommand(@"Update Employees
+                    SET [First Name]=@FName, [Last Name]=@LName, [Middle Name]=@MName, [Initials]=@Initials, [Employee Company]=@ECompany, [Department]=@Department, [Work Location]=@WLocation, [Work Phone Number]=@WPhone, [Manager Name]=@EManager, [Manager Work Location]=@MWLocation, [Manager Work Phone Number]=@MWPhone
+                    WHERE UserID=@UserID;", Connection);
+            //NewEmployee(TextBox3.Text,TextBox1.Text,TextBox2.Text,TextBox4.Text,TextBox5.Text,TextBox6.Text,TextBox7.Text,TextBox8.Text,TextBox9.Text,TextBox10.Text,TextBox11.Text,EmployeeDDL.SelectedItem.Text);
+            Connection.Open();
+            SqlCommand cmd = query;
+            cmd.Parameters.AddWithValue("@LName", sanitizeInput(LName));
+            cmd.Parameters.AddWithValue("@FName", sanitizeInput(FName));
+            cmd.Parameters.AddWithValue("@MName", sanitizeInput(MName));
+            cmd.Parameters.AddWithValue("@Initials", sanitizeInput(Initials));
+            cmd.Parameters.AddWithValue("@UserID", UserID); //one would think 5 characters would be enough to deter sql injection attempts :)
+            cmd.Parameters.AddWithValue("@ECompany", sanitizeInput(ECompany));
+            cmd.Parameters.AddWithValue("@Department", sanitizeInput(Department));
+            cmd.Parameters.AddWithValue("@WLocation", sanitizeInput(WLocation));
+            cmd.Parameters.AddWithValue("@WPhone", sanitizeInput(WPhone));
+            cmd.Parameters.AddWithValue("@MWLocation", sanitizeInput(MWLocation));
+            cmd.Parameters.AddWithValue("@MWPhone", sanitizeInput(MWPhone));
+            if (EManager == "No Manager") //if the 'No Manager' option is selected then Manager Name in database is empty
+            {
+                cmd.Parameters.AddWithValue("@EManager", "");
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@EManager", sanitizeInput(EManager));
+            }
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+
 }
--- EditEmployeeForm.aspx.cs
+++ EditEmployeeForm.aspx.cs
@@ -53,71 +53,17 @@ public partial class EditEmployeeForm : System.Web.UI.Page
     protected void SubmmitButton_Click(object sender, EventArgs e) //save button
     {
         Methods m = new Methods(); //Contains useful methods we can use like santizing input
-
         if (TextBox1.Text != "" && TextBox2.Text != "" && TextBox3.Text != "" && TextBox4.Text != "" && TextBox5.Text != "" && TextBox6.Text != "" && TextBox7.Text != "" && TextBox8.Text != "" && TextBox9.Text != "" && EmployeeDDL.SelectedItem != null && TextBox5.ReadOnly == false)
-        {
-            using (SqlConnection Connection = new SqlConnection("Data Source=badgerequest.cthyx0iu4w46.us-east-2.rds.amazonaws.com;Initial Catalog=badge_request;User ID=pwndatnerd;Password=AaronDavidRandall!3"))
-            {
-                Connection.Open();
-                SqlCommand cmd = new SqlCommand(@"INSERT INTO Employees
-                    VALUES (@LName, @FName, @MName, @Initials, @UserID, @ECompany, @Department, @WLocation, @WPhone, @EManager, @MWLocation, @MWPhone);", Connection);
-                cmd.Parameters.AddWithValue("@LName", m.sanitizeInput(TextBox3.Text));
-                cmd.Parameters.AddWithValue("@FName", m.sanitizeInput(TextBox1.Text));
-                cmd.Parameters.AddWithValue("@MName", m.sanitizeInput(TextBox2.Text));
-                cmd.Parameters.AddWithValue("@Initials", m.sanitizeInput(TextBox4.Text));
-                cmd.Parameters.AddWithValue("@UserID", TextBox5.Text); //one would think 5 characters would be enough to deter sql injection attempts :)
-                cmd.Parameters.AddWithValue("@ECompany", m.sanitizeInput(TextBox6.Text));
-                cmd.Parameters.AddWithValue("@Department", m.sanitizeInput(TextBox7.Text));
-                cmd.Parameters.AddWithValue("@WLocation", m.sanitizeInput(TextBox8.Text));
-                cmd.Parameters.AddWithValue("@WPhone", m.sanitizeInput(TextBox9.Text));
-                cmd.Parameters.AddWithValue("@MWLocation", m.sanitizeInput(TextBox10.Text));
-                cmd.Parameters.AddWithValue("@MWPhone", m.sanitizeInput(TextBox11.Text));
-                if (EmployeeDDL.SelectedItem.Text == "No Manager") //if the 'No Manager' option is selected then Manager Name in database is empty
-                {
-                    cmd.Parameters.AddWithValue("@EManager", "");
-                }
-                else
-                {
-                    cmd.Parameters.AddWithValue("@EManager", EmployeeDDL.SelectedItem.Text);
-                }
-                cmd.ExecuteNonQuery();
-            }
+        {/*New Employee Case*/
+            m.EditEmployee("NEW", TextBox3.Text, TextBox1.Text, TextBox2.Text, TextBox4.Text, TextBox5.Text, TextBox6.Text, TextBox7.Text, TextBox8.Text, TextBox9.Text, TextBox10.Text, TextBox11.Text, EmployeeDDL.SelectedItem.Text);
             Response.Redirect("~/HRForm.aspx");
         }
         if (TextBox1.Text != "" && TextBox2.Text != "" && TextBox3.Text != "" && TextBox4.Text != "" && TextBox6.Text != "" && TextBox7.Text != "" && TextBox8.Text != "" && TextBox9.Text != "" && EmployeeDDL.SelectedItem != null && TextBox5.ReadOnly == true)
-        {
-            //This is our Update button case
-            using (SqlConnection Connection = new SqlConnection("Data Source=badgerequest.cthyx0iu4w46.us-east-2.rds.amazonaws.com;Initial Catalog=badge_request;User ID=pwndatnerd;Password=AaronDavidRandall!3"))
-            {
-                Connection.Open();
-                SqlCommand cmd = new SqlCommand(@"Update Employees
-                    SET [First Name]=@FName, [Last Name]=@LName, [Middle Name]=@MName, [Initials]=@Initials, [Employee Company]=@ECompany, [Department]=@Department, [Work Location]=@WLocation, [Work Phone Number]=@WPhone, [Manager Name]=@EManager, [Manager Work Location]=@MWLocation, [Manager Work Phone Number]=@MWPhone
-                    WHERE UserID=@UserID;", Connection);
-                cmd.Parameters.AddWithValue("@LName", m.sanitizeInput(TextBox3.Text));
-                cmd.Parameters.AddWithValue("@FName", m.sanitizeInput(TextBox1.Text));
-                cmd.Parameters.AddWithValue("@MName", m.sanitizeInput(TextBox2.Text));
-                cmd.Parameters.AddWithValue("@Initials", m.sanitizeInput(TextBox4.Text));
-                cmd.Parameters.AddWithValue("@UserID", TextBox5.Text); //one would think 5 characters would be enough to deter sql injection attempts :)
-                cmd.Parameters.AddWithValue("@ECompany", m.sanitizeInput(TextBox6.Text));
-                cmd.Parameters.AddWithValue("@Department", m.sanitizeInput(TextBox7.Text));
-                cmd.Parameters.AddWithValue("@WLocation", m.sanitizeInput(TextBox8.Text));
-                cmd.Parameters.AddWithValue("@WPhone", m.sanitizeInput(TextBox9.Text));
-                cmd.Parameters.AddWithValue("@MWLocation", m.sanitizeInput(TextBox10.Text));
-                cmd.Parameters.AddWithValue("@MWPhone", m.sanitizeInput(TextBox11.Text));
-                if (EmployeeDDL.SelectedItem.Text == "No Manager") //if the 'No Manager' option is selected then Manager Name in database is empty
-                {
-                    cmd.Parameters.AddWithValue("@EManager", "");
-                }
-                else
-                {
-                    cmd.Parameters.AddWithValue("@EManager", EmployeeDDL.SelectedItem.Text);
-                }
-                cmd.ExecuteNonQuery();
-            }
+        {/*Update Employee Case*/
+            m.EditEmployee("UPDATE", TextBox3.Text, TextBox1.Text, TextBox2.Text, TextBox4.Text, TextBox5.Text, TextBox6.Text, TextBox7.Text, TextBox8.Text, TextBox9.Text, TextBox10.Text, TextBox11.Text, EmployeeDDL.SelectedItem.Text);
             Response.Redirect("~/HRForm.aspx");
         }
     }
-
 
 
     protected void EmployeeDDL_Load(object sender, EventArgs e)
