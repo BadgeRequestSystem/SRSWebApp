ce2969ba2051d4346fcfeef4ed86b00fa52f1f63
Aaron Prather <aaprather@augusta.edu>
2018-05-21 15:44:59 -0400

    Now calling methods from Methods.cs and whenever a user ends up back on the login page, all possible cookies will be deleted, which will reset their session. Login.aspx.cs has bee cleaned up A LOT :)

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,122 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Net;
-using System.Security.Cryptography;
 
 public partial class Login : System.Web.UI.Page
 {
+    public static Methods m = new Methods();
 
 
     protected void Page_Load(object sender, EventArgs e)
     {
         ClientScript.RegisterStartupScript(this.GetType(), "script", "LoginFadeIn();", true); //fade effects script (see JS.js file)
-        /*
-        ************************Currently just used to generate password hashes*************************
-        byte[] salt;
-        new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
-        System.Security.Cryptography.Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes("Adams", salt, 10000);
-        byte[] hash = pbkdf2.GetBytes(20);
-        byte[] hashBytes = new byte[36];
-        Array.Copy(salt, 0, hashBytes, 0, 16);
-        Array.Copy(hash, 0, hashBytes, 16, 20);
-        string savedPasswordHash = Convert.ToBase64String(hashBytes);
-        userBox.Text = savedPasswordHash; //+ " Salt:" + Convert.ToBase64String(salt);
-        *************************************************************************************************
-        */
-
+        /*If user somehow ends up back on Login page, lets make them log back in and reset their session */
+        /*REMOVE EVERY POSSIBLE COOKIE THAT MAY HAVE BEEN RECEIVED TO PREVENT ERRORS*/
+        m.DeleteCookie("userInfo"); 
+        m.DeleteCookie("USERname");
+        m.DeleteCookie("submittedCookieInfo");
+        m.DeleteCookie("Manager");
+        m.DeleteCookie("draftInfo");
+        m.DeleteCookie("EmployeeToDelete");
+        m.DeleteCookie("selectedEmployee");
+        /*****************************************************************************/
     }
 
     protected void LoginBtn_Click(object sender, EventArgs e)
     {
-        Methods m = new Methods();
-        bool isManager = false;
-        bool isHR = false;
-        string tempID;
-        string tempMan;
-        string tempEmail;
-        using (SqlConnection Connection = new SqlConnection("Data Source=badgerequest.cthyx0iu4w46.us-east-2.rds.amazonaws.com;Initial Catalog=badge_request;User ID=pwndatnerd;Password=AaronDavidRandall!3"))
+        HttpCookie aCookie = new HttpCookie("userInfo");
+        Response.Cookies.Add(aCookie);
+        m.checkLogin(aCookie, userBox.Text, passBox.Text); /*We will check the login info, and if the password hashes correctly, 
+                                                   and the username matches; then we will fill the 'userInfo' cookie and send them to the MainMenuForm.*/
+        if(aCookie.Values["isManager"] != null && aCookie.Values["isManager"] != "") //If its not null or empty, then login was succesful, so redirect to MainMenuForm
+            Response.Redirect("~/MainMenuForm.aspx");
+        else
         {
-            try
-            {
-                Connection.Open();
-
-                SqlCommand cmd = new SqlCommand(@"Select PasswordHash FROM Credentials   WHERE Username=@uname", Connection);
-                cmd.Parameters.AddWithValue("@uname", m.sanitizeInput(userBox.Text));
-
-                string temp2 = (string)cmd.ExecuteScalar();
-                byte[] hashBytes = Convert.FromBase64String(temp2);
-                byte[] salt = new byte[16];
-                Array.Copy(hashBytes, 0, salt, 0, 16);
-                System.Security.Cryptography.Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(passBox.Text, salt, 10000);
-                byte[] hash = pbkdf2.GetBytes(20);
-                int result = 1;
-                for (int i = 0; i < 20; i++)
-                    if (hashBytes[i + 16] != hash[i])
-                    {
-                        result = 0;
-                    }
-
-                if (result == 1)
-                {
-                    //logged in succesfully, storing isManager and isHR to fields and then to cookie
-                    SqlCommand cmdGetManager = new SqlCommand(@"SELECT isManager FROM Credentials 
-                                                    WHERE Username=@uname", Connection);
-                    cmdGetManager.Parameters.AddWithValue("@uname", userBox.Text);
-                    bool temp = (bool)cmdGetManager.ExecuteScalar();
-                    isManager = temp;
-
-                    SqlCommand cmdGetHR = new SqlCommand(@"SELECT isHR FROM Credentials 
-                                                    WHERE Username=@uname", Connection);
-                    cmdGetHR.Parameters.AddWithValue("@uname", userBox.Text);
-                    temp = (bool)cmdGetHR.ExecuteScalar();
-                    isHR = temp;
-                    
-                    SqlCommand cmdGetUserID = new SqlCommand(@"SELECT UserID FROM Credentials 
-                                                    WHERE Username=@uname", Connection);
-                    cmdGetUserID.Parameters.AddWithValue("@uname", userBox.Text);
-                    tempID = (string)cmdGetUserID.ExecuteScalar();
-
-                    SqlCommand cmdGetManagerName = new SqlCommand(@"SELECT [Manager Name] FROM Employees
-                                                    WHERE UserID=@tempID", Connection);
-                    cmdGetManagerName.Parameters.AddWithValue("@tempID", tempID);
-                    tempMan = (string)cmdGetManagerName.ExecuteScalar();
-
-                    SqlCommand cmdGetEmail = new SqlCommand(@"SELECT Email FROM Credentials
-                                                    WHERE Username=@uname", Connection);
-                    cmdGetEmail.Parameters.AddWithValue("@uname", userBox.Text);
-                    tempEmail = (string)cmdGetEmail.ExecuteScalar();
-
-                    HttpCookie aCookie = new HttpCookie("userInfo");
-                    aCookie.Values["userName"] = userBox.Text;
-                    aCookie.Values["isManager"] = isManager.ToString();
-                    aCookie.Values["isHR"] = isHR.ToString();
-                    aCookie.Values["UserID"] = tempID;
-                    aCookie.Values["Manager"] = tempMan;
-                    aCookie.Values["Email"] = tempEmail;
-                    Response.Cookies.Add(aCookie);
-
-                    Connection.Close();
-                    Response.Redirect("~/MainMenuForm.aspx");
-
-                }
-
-                else //LOGIN UNSUCCESFUL
-                {
-                    Connection.Close();
-                    Response.Redirect("~/Login.aspx");
-
-                }
-                    
-            }
-            catch (Exception ex)
-            {
-
-            }
+            
+            Response.Redirect("~/Login.aspx"); //Cookie not filled, login was not succesful so reload Login page
         }
-
+            
     }
 }
